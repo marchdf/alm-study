ab0ad6d79f9921d66b210d924efe22fb0bcc189c
Marc Henry de Frahan <marc.henrydefrahan@nrel.gov>
2023-12-22 09:54:39 -0700

    add refdata

--- alm_study/plotter.py
+++ alm_study/plotter.py
@@ -1,15 +1,35 @@
 """Plot ALM data."""
 
-import os
+import argparse
+import inspect
+import itertools
+import pathlib
 
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.backends.backend_pdf import PdfPages
 
+import alm_study as alm
 import alm_study.read_data as rd
+
+module_dir = pathlib.Path(inspect.getfile(alm)).parents[0]
+base_dir = module_dir.parents[0]
+plt.style.use(module_dir / "project.mplstyle")
+plt.rcParams.update({"figure.max_open_warning": 0})
+prop_cycle = plt.rcParams["axes.prop_cycle"]
+cmap = prop_cycle.by_key()["color"]
+linestyles = prop_cycle.by_key()["linestyle"]
+markers = itertools.cycle(("s", "d", "o", "p", "h"))
 
 
 def main():
     """Plot data."""
+    parser = argparse.ArgumentParser(description="A simple plot tool")
+    parser.add_argument(
+        "-f", "--fdir", help="Folder with amr-wind data files", required=True, type=str
+    )
+    args = parser.parse_args()
+
     uref = 8
     dref = 126
     # Force scale
@@ -33,88 +53,53 @@ def main():
     ]
 
     # The location of all cases
-    fname = (
-        "/lustre/eaglefs/scratch/lmartine/amrwind_alm_nrel5MW_2/fast_inp/nrel5mw.out"
-    )
+    fdir = pathlib.Path(args.fdir)
+    fname = fdir / "fast_inp" / "/nrel5mw.out"
 
-    # The times to average
-    times = [500, 1300]
-    # times = [0.2, 0.2]
+    # # The list of all the cases
+    # times = [500, 1300]
+    # case = rd.CaseClass(
+    #     fname=fname,
+    #     label="amr-wind",
+    #     qoi_ls=qoi_ls,
+    #     times=times,
+    #     marker="o",
+    # )
 
-    # The list of all the cases
-    cases = [
-        rd.CaseClass(
-            fname=fname,
-            label="amr-wind",
-            qoi_ls=qoi_ls,
-            times=times,
-            marker="o",
-        ),
-    ]
-
-    out_dir = "alm_plots"
-    if not os.path.exists(out_dir):
-        os.makedirs(out_dir)
+    refdir = base_dir / "refdata"
+    refnames = {
+        "JHU": {"label": "Johns Hopkins Code"},
+        "KUL": {"label": "KU Leuven Code"},
+        "NREL": {"label": "SOWFA"},
+    }
+    refmap = {"Vx": "Vaxial", "Alpha": "alpha", "Fl": "lift", "Fd": "drag"}
 
     # Loop and plot all the qoi along the blade
-    for qoi in qoi_ls:
+    fname = "plots.pdf"
+    with PdfPages(fname) as pdf:
+        for qoi in qoi_ls:
+            plt.figure(qoi.index)
+            # plt.plot(
+            #     case.r,
+            #     case.qoi[i][qoi.index],
+            #     marker=case.marker,
+            #     # markevery=4,
+            #     ms=6,
+            #     ls=case.ls,
+            #     # color=case.color,
+            #     label="AMR-Wind",
+            # )
 
-        plt.clf()
-        # plt.figure(figsize=(16,6))
+            for k, v in refnames.items():
+                x, y = np.loadtxt(refdir / f"{k}_Data_{refmap[qoi.index]}", unpack=True)
+                plt.plot(x, y, "o-", label=v["label"])
 
-        # Loop thorugh all blades
-        for i in range(1):
+            # rd.draw_blade("/home/lmartine/blade/blade_nrel5mw.png")
 
-            plt.clf()
-
-            # Loop through all cases
-            for case in cases:
-
-                plt.plot(
-                    case.r,
-                    case.qoi[i][qoi.index],
-                    marker=case.marker,
-                    # markevery=4,
-                    ms=6,
-                    ls=case.ls,
-                    # color=case.color,
-                    label="AMR-Wind",
-                )
-
-            #         if qoi.index=='AOA' or qoi.index == 'Phi':
-            #             plt.ylim([4, 15])
-            legend = {
-                "JHU": "Johns Hopkins Code",
-                "KUL": "KU Leuven Code",
-                "NREL": "SOWFA",
-            }
-
-            for vars in [
-                ["Vx", "Vaxial"],
-                ["Alpha", "alpha"],
-                ["Fl", "lift"],
-                ["Fd", "drag"],
-            ]:
-                if qoi.index == vars[0]:
-                    for label in ["JHU", "KUL", "NREL"]:
-                        x, y = np.loadtxt(
-                            "shareData/" + label + "_Data_" + vars[1], unpack=True
-                        )
-                        plt.plot(x, y, "o-", label=legend[label])
-
-            rd.draw_blade("/home/lmartine/blade/blade_nrel5mw.png")
-
-            xlabel = "non-dimensional blade radius [-]"
-            ylabel = qoi.label
-
-            plt.grid()
-            plt.xlabel(xlabel)
-            plt.ylabel(ylabel)
-            plt.legend(bbox_to_anchor=(1.02, 0.5), loc="center left", ncol=1)
-
-            plt.savefig(os.path.join(out_dir, qoi.index + ".pdf"), bbox_inches="tight")
-
-            plt.show()
+            plt.xlabel(r"$r[-]$")
+            plt.ylabel(qoi.label)
+            plt.gca().legend()
+            pdf.savefig()
 
 
 if __name__ == "__main__":
